5a3f36b95eaa5108e001ce7643f69e7ca662f57e
Devang Joshi <devangjoshi11@gmail.com>
2021-02-06 01:15:31 +0530

    Made Functioning

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const calculatorScreen = document.querySelector('.calculator-screen')
+const calculatorScreen = document.querySelector('.calculatorScreen');
+
+window.document.addEventListener("mousedown", function (event) {
+    event.preventDefault();
+});
 
 const updateScreen = (number) => {
-    calculatorScreen.value = number
+    calculatorScreen.value = number;
 }
-const numbers = document.querySelectorAll(".number")
+
+const numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
         inputNumber(event.target.value);
-        updateScreen(currentInput)
+        updateScreen(currentInput);
     })
 })
 
@@ -16,57 +21,127 @@ let calculationOperator = '';
 let currentInput = '0';
 const inputNumber = (number) => {
     if (currentInput === '0') {
-        currentInput = number
+        currentInput = number;
     } else {
-        currentInput += number
+        currentInput += number;
     }
 }
 
-const operators = document.querySelectorAll('.operator')
+const operators = document.querySelectorAll('.operator');
 operators.forEach((operator) => {
     operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value)
+        inputOperator(event.target.value);
     })
 })
+
 const inputOperator = (operator) => {
-    prevInput = currentInput
-    calculationOperator = operator
-    currentInput = '0'
+    if (calculationOperator == '') {
+        prevInput = currentInput;
+        calculationOperator = operator;
+        currentInput = '0';
+    } else {
+        calculationOperator = operator;
+    }
 }
 
-const equalSign = document.querySelector('.equal-sign')
+const equalSign = document.querySelector('.equalSign');
 equalSign.addEventListener('click', () => {
-    calculate()
-    updateScreen(currentInput)
+    if (currentInput !== typeof currentInput) {
+        calculate();
+        updateScreen(currentInput);
+    }
 })
+
 const calculate = () => {
-    let result = 0
+    let result = 0;
     switch (calculationOperator) {
         case '+':
-            result = parseInt(prevInput) + parseInt(currentInput)
-            break
+            result = Number(prevInput) + Number(currentInput);
+            break;
         case '-':
-            result = parseInt(prevInput) - parseInt(currentInput)
-            break
+            result = Number(prevInput) - Number(currentInput);
+            break;
         case '*':
-            result = parseInt(prevInput) * parseInt(currentInput)
-            break
+            result = Number(prevInput) * Number(currentInput);
+            break;
         case '/':
-            result = parseInt(prevInput) / parseInt(currentInput)
-            break
+            result = Number(prevInput) / Number(currentInput);
+            break;
+        case '%':
+            result = Number(prevInput) % Number(currentInput);
+            break;
         default:
             return
     }
-    currentInput = result.toString()
-    calculationOperator = ''
+    currentInput = result.toString();
+    calculationOperator = '';
 }
-const clearBtn = document.querySelector('.all-clear')
-clearBtn.addEventListener('click', () => {
-    clearAll()
-    updateScreen(currentInput)
-})
+
 const clearAll = () => {
-    prevInput = '0'
-    calculationOperator = ''
-    currentInput = '0'
+    prevInput = '0';
+    calculationOperator = '';
+    currentInput = '0';
 }
+
+const clearBtn = document.querySelector('.allClear');
+
+clearBtn.addEventListener('click', () => {
+    clearAll();
+    updateScreen(currentInput);
+})
+
+const deleteChar = document.querySelector('.delete');
+
+deleteChar.addEventListener('click', () => {
+    currentInput = calculatorScreen.value.slice(0, -1);
+    updateScreen(currentInput);
+})
+
+const decimal = document.querySelector(".decimal");
+
+decimal.addEventListener('click', () => {
+    if (!currentInput.includes(".")) {
+        currentInput += ".";
+        updateScreen(currentInput);
+    }
+})
+
+document.addEventListener("keydown", function (event) {
+    event.preventDefault();
+    var key = event.key;
+
+    if (event.which === 32) {
+        return false;
+    }
+    if (key === "Enter") {
+        equalSign.click();
+    }
+    if (key === "Delete") {
+        clearBtn.click();
+    }
+    if (event.which === 8) {
+        deleteChar.click();
+    }
+    if (key >= 0 && key <= 9) {
+        inputNumber(key);
+        updateScreen(currentInput);
+    }
+    if (key === "/") {
+        inputOperator(key);
+    }
+    if (key === "*") {
+        inputOperator(key);
+    }
+    if (key === "-") {
+        inputOperator(key);
+    }
+    if (key === "+") {
+        inputOperator(key);
+    }
+    if (key === "%") {
+        inputOperator(key);
+    }
+    if (key === ".") {
+        decimal.click();
+    }
+});
