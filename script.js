135012075d693ac8d303a71aacf8f1f38aaacffa
Devang Joshi <devangjoshi11@gmail.com>
2021-02-06 16:26:50 +0530

    Added Comments and More Functioning

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+'use strict';
+
+// following are the variables to be used throughout the application code
 const calculatorScreen = document.querySelector('.calculatorScreen');
-
-window.document.addEventListener("mousedown", function (event) {
-    event.preventDefault();
-});
-
-const updateScreen = (number) => {
-    calculatorScreen.value = number;
-}
-
+const clearBtn = document.querySelector('.allClear');
+const deleteChar = document.querySelector('.delete');
+const operators = document.querySelectorAll('.operator');
 const numbers = document.querySelectorAll(".number");
-numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {
-        inputNumber(event.target.value);
-        updateScreen(currentInput);
-    })
-})
+const decimal = document.querySelector(".decimal");
+const equalSign = document.querySelector('.equalSign');
 
 let prevInput = '0';
 let calculationOperator = '';
 let currentInput = '0';
+
+window.document.addEventListener("mousedown", function (event) {
+    // function to prevent user from selecting any text
+    event.preventDefault();
+});
+
+const updateScreen = (number) => {
+    // function to update Calculator Screen with any specified value
+    calculatorScreen.value = number;
+}
+
 const inputNumber = (number) => {
+    // function to update current currentInput variable with the value we wants to write by clicking or typing which will be used by updateScreen function to update Calculator Screen
     if (currentInput === '0') {
         currentInput = number;
     } else {
@@ -27,32 +32,16 @@ const inputNumber = (number) => {
     }
 }
 
-const operators = document.querySelectorAll('.operator');
-operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value);
-    })
-})
-
 const inputOperator = (operator) => {
-    if (calculationOperator == '') {
-        prevInput = currentInput;
-        calculationOperator = operator;
-        currentInput = '0';
-    } else {
-        calculationOperator = operator;
-    }
+    // function to perform operations on values according to the desired operator and update Calculator Screen
+    equalSign.click();
+    prevInput = currentInput;
+    calculationOperator = operator;
+    currentInput = '';
 }
 
-const equalSign = document.querySelector('.equalSign');
-equalSign.addEventListener('click', () => {
-    if (currentInput !== typeof currentInput) {
-        calculate();
-        updateScreen(currentInput);
-    }
-})
-
 const calculate = () => {
+    // function to perform operations on values according to the desired operator and return answer to currentInput variable
     let result = 0;
     switch (calculationOperator) {
         case '+':
@@ -78,35 +67,57 @@ const calculate = () => {
 }
 
 const clearAll = () => {
+    // function to clear everything from Screen and variable values
     prevInput = '0';
     calculationOperator = '';
     currentInput = '0';
 }
 
-const clearBtn = document.querySelector('.allClear');
-
-clearBtn.addEventListener('click', () => {
-    clearAll();
-    updateScreen(currentInput);
-})
-
-const deleteChar = document.querySelector('.delete');
-
 deleteChar.addEventListener('click', () => {
+    // function to delete last character from Calculator Screen
     currentInput = calculatorScreen.value.slice(0, -1);
     updateScreen(currentInput);
-})
+});
 
-const decimal = document.querySelector(".decimal");
+clearBtn.addEventListener('click', () => {
+    // function to to execute clearAll function and update Calculator Screen with default value
+    clearAll();
+    updateScreen(currentInput);
+});
+
+numbers.forEach((number) => {
+    // function to perform inputNumber function on every click on number buttons
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value);
+        updateScreen(currentInput);
+    })
+});
+
+operators.forEach((operator) => {
+    // function to perform inputOperator function on every click on operator buttons
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value);
+    })
+});
+
+equalSign.addEventListener('click', () => {
+    // function to perform calculations when clicked on equal sign button
+    if (currentInput !== typeof currentInput) {
+        calculate();
+        updateScreen(currentInput);
+    }
+});
 
 decimal.addEventListener('click', () => {
+    // function to append Calculator Screen with decimal point if it doesn't exist already when clicked on decimal button
     if (!currentInput.includes(".")) {
         currentInput += ".";
         updateScreen(currentInput);
     }
-})
+});
 
 document.addEventListener("keydown", function (event) {
+    // function to perform clickable operations on certain buttons when some specified key is pressed
     event.preventDefault();
     var key = event.key;
 
